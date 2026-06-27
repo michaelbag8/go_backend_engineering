25c4068edeff32f30e24d5c42986d066fbe8490c
michaelbag8 <michaelbag8@gmail.com>
2026-06-27 11:09:13 +0100

    go mod added

--- quest1.go
+++ quest1.go
@@ -1,1 +1,18 @@
-//Understanding Go toolchain
+// Understanding Go toolchain
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func myName(name, message string, date string) (string, string, time.Time) {
+	return name, message, time.Now()
+}
+func main() {
+	name := "Michael Bag"
+	message := "Backend engineering journey begins."
+	date := time.Now().Format("2006-01-02")
+	fmt.Println(myName(name, message, date))
+
+}
