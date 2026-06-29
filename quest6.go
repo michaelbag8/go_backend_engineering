99e41bed86c4d1228eec1559ec44c922277c6fb3
michaelbag8 <michaelbag8@gmail.com>
2026-06-29 09:21:57 +0100

    Add: quest6

--- quest6.go
+++ quest6.go
@@ -1,2 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+type User struct {
+	Name  string
+	Email string
+	Age   int
+}
+
+func (u User) Describe() string {
+	return fmt.Sprintf("%s (%s) -- %d", u.Name, u.Email, u.Age)
+}
+
+func main() {
+	user := User{
+		Name:  "Michael Bag",
+		Email: "michaelbag8@gmail.com",
+		Age:   303,
+	}
+
+	fmt.Println(user.Describe())
+}
